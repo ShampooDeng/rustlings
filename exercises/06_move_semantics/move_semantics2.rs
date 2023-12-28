af3aed124528964177aba04dcbd9ab0ef65b6230
ShampooDeng <lwxkkdy@foxmail.com>
2023-12-28 20:45:54 +0800

    2023/12/28

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -5,13 +5,17 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// NOTE: fill(vec0) will move the data owned by vec0 to vec inside fill_vec.
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    // let mut vec1 = fill_vec(vec0.clone());
+    // Another way:
+    let mut vec2: Vec<i32> = Vec::new();
+    vec0.clone_into(&mut vec2);
+    let mut vec1 = fill_vec(vec2);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
