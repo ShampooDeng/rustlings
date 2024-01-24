32f3d495030530d52af450d59011335efd1a41f9
ShampooDeng <lwxkkdy@foxmail.com>
2024-01-24 22:58:03 +0800

    2024/01/24

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -15,7 +15,13 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let mut cost = 0i32;
+    let ret = total_cost(pretend_user_input);
+    if let Err(err) = ret {
+        println!("{}", err);
+    } else if let Ok(c) = ret {
+        cost = c;
+    }
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,6 +34,7 @@ fn main() {
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
+    // ^ cannot use the `?` operator in a function that returns `()`
     let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
